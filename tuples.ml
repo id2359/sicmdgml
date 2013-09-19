ccda309ccea5b4b80ba79414e6f3adb8e7df7a78
Lee Render <id2359@yahoo.com>
2013-09-19 18:00:27 +0800

    Added tuple structure

---
+++ tuples.ml
@@ -0,0 +1,19 @@
+type nums = float list
+type tuple = 
+	Up of nums | 
+	Down of nums | 
+	UpStructure of tuple list |
+	DownStructure of tuple list 
+
+let up xs = Up xs
+let down xs = Down xs
+
+let up1 x = up [x]
+let down1 x = down [x]
+
+let rec cc a b = function
+	|  (Up xs ,  Down ys) 
+	|  (Down xs, Up ys) -> ( List.length xs ) == ( List.length ys )
+	| _  -> false
+
+
